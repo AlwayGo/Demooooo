4262c32ea014581404742fa8298cfd16d1d8d2c0
User <memmedlinihad103@gmail.com>
2025-02-25 14:46:16 +0300

    Login ekranı oluşturuldu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+//
     //edited
+    //
+    //
+    ///
+
+    ////edited
 }
